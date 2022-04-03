5f603fb40bbbeb4f348cf65fbbeee9d08d63ac4f
El-Peasant <102629743+El-Peasant@users.noreply.github.com>
2022-04-03 13:14:47 -0400

    Update Bio.tsx

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -10,15 +10,16 @@ const Bio = () => {
             <div className="help-content">
                 <BioBox>
                     <BaseLabel>
-                        Born with a passion for video games, I pursued an artistic instruction here in Montreal/Quebec, specializing in the branch of 3d art and level design.
+                        I was born in 1989 with a passion for gaming and video games. It’s only later on, near my 30’s that I decided to pursue an artistic instruction here in Montreal/Quebec, specializing in the branch of 3d art and level design.
                     </BaseLabel>
                     <br />
                     <BaseLabel>
-                        Before that, I used to work as a mixologist and compete in a few regional and national competitions in cocktail crafting. Those past years in the restoration industries gave me skills and psychological knowledge to push my creative mind and work under pressure.
+                        Before that, I used to work as a mixologist and compete in a few regional and national competitions in cocktail crafting. Those past years in the restoration industries gave me skills and psychological knowledge to push my creative mind and work under pressure. Through these years I have gained experience through various computer software such as Acomba, Excell, Photoshop, etc.
+In 2020, I decided to change careers and leave the restaurant business. I now work in the field of video games. I am currently QA at Warner Brother in Montreal.
                     </BaseLabel>
                     <br />
                     <BaseLabel>
-                        Professionally speaking I work in the fields of 3d artist, loving accessories modeling, environment and design.
+                        I am pushing my learnings  in  3d softwares, like 3ds Max, Blender, Zbrush, Unreal Engine, Substance Painter and Marmoset.
                     </BaseLabel>
                     <br />
                     <BaseLabel>
