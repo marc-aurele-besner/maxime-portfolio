6a85d7b1d70ae3ab1e552643d8dd24254b9db9f8
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2022-04-04 18:21:23 -0400

    Add header, menu and social icons

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 // import { Link } from "react-router-dom";
 import styled from 'styled-components'
-import Typewriter from 'typewriter-effect'
+// import Typewriter from 'typewriter-effect'
 
 import Bio from '../components/Bio'
 import ContactUs from '../components/ContactUs'
@@ -15,6 +15,20 @@ const StyledHome = styled.div`
   overflow:scroll;
   overflow-x:hidden;
 `
+
+const Menu = styled.a`
+  color: #fff;
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin: 1rem;
+  text-decoration: none;
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    color: #fff;
+    text-decoration: underline;
+  }
+`
+
 const StyledTitle = styled.h1`
   margin-top: 2vh;
   color: #fff;
@@ -41,13 +55,28 @@ const Center = styled.div`
 //   }
 // `
 
-const StyledTypewriter = styled.div`
-  color: #fff;
-  font-size: 1.5rem;
-  height: 10vh;
+// const StyledTypewriter = styled.div`
+//   color: #fff;
+//   font-size: 1.5rem;
+//   height: 10vh;
 
-  &:hover {
-    color: #adadad;
+//   &:hover {
+//     color: #adadad;
+//   }
+// `
+
+const MenuGrid = styled.div`
+  display: grid;
+  padding-left: 20px;
+  grid-gap: 20px;
+  width: 70vw;
+  grid-template-columns: repeat(4, 1fr);
+  padding-bottom: 12px;
+  padding-top: 12px;
+  transition: all 0.2s ease-in-out;
+
+  @media only screen and (max-width: 600px) {
+    grid-template-columns: repeat(1, 1fr);
   }
 `
 
@@ -57,6 +86,7 @@ const ModelsGrid = styled.div`
   grid-template-columns: repeat(3, 1fr);
   padding-bottom: 24px;
   padding-top: 24px;
+  transition: all 0.2s ease-in-out;
 
   @media only screen and (max-width: 600px) {
     grid-template-columns: repeat(1, 1fr);
@@ -77,14 +107,90 @@ const Content = styled.div`
   flex: 1;
 `
 
+const HeaderImage = styled.div`
+  background-image: url('/images/header.jpg');
+  position: relative;
+  width: 1366px;
+  height: 80vh;
+`
+const SocialMedia = styled.div`
+  top: 26vh;
+  right: 10vw;
+  position: absolute;
+  padding: 2px;
+  @media only screen and (max-width: 600px) {
+    top: 26vh;
+    left: 20px;
+    right: unset;
+  }
+`
+
+const SocialMediaLink = styled.a`
+  color: #fff;
+  font-size: 1.5rem;
+  margin: 1rem;
+  text-decoration: none;
+  transition: all 0.2s ease-in-out;
+  padding: 0px;
+  margin: 5px;
+  &:hover {
+    color: #fff;
+    text-decoration: underline;
+  }
+`
+
+const Icone = styled.img`
+  width: 30px;
+  height: 30px;
+  margin-left: 0px;
+  margin-right: 0px;
+  padding: 0px;
+`
+
 const Home: React.FC = () => {
 
   return (
     <StyledHome>
       <Center>
         <br />
+        <HeaderImage id="Home">
+          <SocialMedia>
+            <SocialMediaLink href="https://www.artstation.com/el_peasant">
+              <Icone src="images/artStation.png" />
+            </SocialMediaLink>
+            <SocialMediaLink href="https://www.instagram.com/sublime_peasant">
+              <Icone src="images/instagram.png" />
+            </SocialMediaLink>
+            <SocialMediaLink href="https://www.linkedin.com/in/maxime-lafreniere-5a44a0236/">
+              <Icone src="images/linkedin.png" />
+            </SocialMediaLink>
+          </SocialMedia>
+        </HeaderImage>
         <br />
-        <StyledTitle>Maxime</StyledTitle>
+        
+        <MenuGrid>
+          <Content>
+            <Menu href="#Home">
+              <StyledTitle>Home</StyledTitle>
+            </Menu>
+          </Content>
+          <Content>
+            <Menu href="#Bio">
+              <StyledTitle>Bio</StyledTitle>
+            </Menu>
+          </Content>
+          <Content>
+            <Menu href="#Gallery">
+              <StyledTitle>Gallery</StyledTitle>
+            </Menu>
+          </Content>
+          <Content>
+            <Menu href="#Contact">
+              <StyledTitle>Contact</StyledTitle>
+            </Menu>
+          </Content>
+        </MenuGrid>
+        {/* <StyledTitle>Maxime</StyledTitle>
        <StyledTypewriter>
           <Typewriter
             onInit={(typewriter) => {
@@ -101,10 +207,10 @@ const Home: React.FC = () => {
               loop: true,
             }}
           />
-        </StyledTypewriter>
+        </StyledTypewriter> */}
         <br />
         <br />
-        <ModelsGrid>
+        <ModelsGrid id="Gallery">
           <Content>
             <Image src='/images/Gonk.png' alt='Gonk' onClick={() => window.open('/gallery/Gonk')} />
           </Content>
@@ -123,10 +229,10 @@ const Home: React.FC = () => {
         </ModelsGrid>
         <br />
         <br />
-        <Bio />
+        <Bio id="Bio" />
         <br />
         <br />
-        <ContactUs />
+        <ContactUs id="Contact" />
         <br />
         <br />
       </Center>
